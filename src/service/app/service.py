9f0d9b42e4b2f7aa0ed3d5034a5fabee81077a6d
Viorel Rinja <viorel.rinja@gmail.com>
2021-10-11 00:38:56 +0300

    add multiple keyword search

--- src/service/app/service.py
+++ src/service/app/service.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_restful import Resource, Api
 from db_processed import Keyword, session_factory
 
-session = session_factory()
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -11,16 +9,18 @@ def flatten_list(l):
   return [item for sublist in l for item in sublist]
 
 def get_urls_related_to_keywords(keywords):
+  session = session_factory()
   urls = flatten_list([k.urls for k in session.query(Keyword).filter(Keyword.keyword.in_(keywords)) ])
+  session.close()
   return [u.url for u in urls]
 
 class HelloWorld(Resource):
-  def get(self, keyword):
+  def get(self, keywords):
     print("Viorel", flush=True)
-    return get_urls_related_to_keywords([keyword])
+    return get_urls_related_to_keywords(keywords.split())
 
 
-api.add_resource(HelloWorld, '/<string:keyword>')
+api.add_resource(HelloWorld, '/<string:keywords>')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
