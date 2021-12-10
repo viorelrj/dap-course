abb6c5a283d10ed15c0f47a62f8309524ad76e3f
Viorel Rinja <viorel.rinja@gmail.com>
2021-12-10 17:58:33 +0200

    add db basic monitoring

---
+++ src/processed_db/init.sh
@@ -0,0 +1,8 @@
+echo "Pidar"
+
+# curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.16.0-amd64.deb
+# dpkg -i metricbeat-7.16.0-amd64.deb && \
+# sed -i 's/localhost/elasticsearch/g' /etc/metricbeat/metricbeat.yml && \
+# metricbeat modules enable postgresql && \
+# metricbeat setup -e
+# service metricbeat start
